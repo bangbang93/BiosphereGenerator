36d7e5a37646e5cdccc4b9d5200c925d6aa2e079
BtbN <btbn@btbn.de>
2012-04-16 21:02:06 +0200

    fix warning

--- src/de/btbn/BiosphereGenerator/Populators/HellPopulator.java
+++ src/de/btbn/BiosphereGenerator/Populators/HellPopulator.java
@@ -6,7 +6,6 @@ import java.util.Random;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.TreeType;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
